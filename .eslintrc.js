a313393b312147e4208a20354d1a45a17f292ae5
@slovacus <slovacus@gmail.com>
2020-04-30 15:59:12 -0500

    refactor: settings for validate dependences packages (#49)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
-const prettierrc = require('@equipindustry/prettierrc');
 module.exports = {
-  parser: 'babel-eslint',
+  extends: ['@equipindustry/eslint-config'],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -8,36 +12,4 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  rules: {
-    'prettier/prettier': [
-      'error',
-      prettierrc,
-      {
-        usePrettierrc: true,
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      env: { browser: true, es6: true, node: true },
-      extends: ['@equipindustry/eslint-config'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-      },
-      plugins: ['prettier', '@typescript-eslint'],
-      rules: {
-        'prettier/prettier': [
-          'error',
-          prettierrc,
-          {
-            usePrettierrc: true,
-          },
-        ],
-      },
-    },
-  ],
 };
