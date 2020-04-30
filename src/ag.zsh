e16bdefbfa5a2b8b1ef28471fdbff9715d7c430d
@slovacus <slovacus@gmail.com>
2020-04-30 16:09:12 -0500

    chore: implement function agr for replace (#49)

---
+++ src/ag.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+# agr - Replace value by new value using silver search
+function agr {
+    ag --hidden --ignore=.git -0 -l "${1}" \
+        | AGR_FROM="${1}" AGR_TO="${2}" xargs -0 perl -pi -e "s/$ENV{AGR_FROM}/$ENV{AGR_TO}/g";
+}
