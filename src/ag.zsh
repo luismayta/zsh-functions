33d0bd3c39d9d74e5fd4317cd1444362926d39d1
@slovacus <slovacus@gmail.com>
2020-05-01 16:20:09 -0500

    fix: function for replace with ag (#49)

--- src/ag.zsh
+++ src/ag.zsh
@@ -4,5 +4,5 @@
 # agr - Replace value by new value using silver search
 function agr {
     ag --hidden --ignore=.git -0 -l "${1}" \
-        | AGR_FROM="${1}" AGR_TO="${2}" xargs -0 perl -pi -e "s/$ENV{AGR_FROM}/$ENV{AGR_TO}/g";
+        | AGR_FROM="${1}" AGR_TO="${2}" xargs -0 perl -pi -e 's/$ENV{AGR_FROM}/$ENV{AGR_TO}/g';
 }
